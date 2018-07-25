b16ae0135467fc105c3e43cf15ac1b4da6e65780
zessirb <zessirb@gmail.com>
2018-07-25 20:41:24 +0200

    Non functional Tests

---
+++ src/test/java/com/onaple/harvester/TestTest.java
@@ -0,0 +1,56 @@
+package com.onaple.harvester;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.spongepowered.api.event.message.MessageChannelEvent;
+import org.spongepowered.mctester.api.junit.MinecraftRunner;
+import org.spongepowered.mctester.internal.BaseTest;
+import org.spongepowered.mctester.internal.event.StandaloneEventListener;
+import org.spongepowered.mctester.junit.TestUtils;
+
+@RunWith(MinecraftRunner.class)
+public class TestTest extends BaseTest {
+
+    @Rule
+    public ExpectedException expectedEx = ExpectedException.none();
+
+    public TestTest(TestUtils testUtils) {
+        super(testUtils);
+    }
+
+    @Test
+    public void testOneShotEventListenerException() throws Throwable {
+        expectedEx.expect(AssertionError.class);
+        expectedEx.expectMessage(CoreMatchers.containsString("Got message: One shot"));
+
+        this.testUtils.listenOneShot(() -> {
+            this.testUtils.getClient().sendMessage("One shot");
+        }, new StandaloneEventListener<>(MessageChannelEvent.Chat.class, (MessageChannelEvent.Chat event) -> Assert.fail("Got message: " + event.getRawMessage().toPlain())));
+    }
+
+    @Test
+    public void testPermanentEventListenerException() throws Throwable {
+        expectedEx.expect(AssertionError.class);
+        expectedEx.expectMessage(CoreMatchers.containsString("Got message: Permanent message"));
+
+        this.testUtils.listen(new StandaloneEventListener<>(MessageChannelEvent.Chat.class, (MessageChannelEvent.Chat event) -> Assert.fail("Got message: " + event.getRawMessage().toPlain())));
+        this.testUtils.getClient().sendMessage("Permanent message");
+    }
+
+    @Test
+    public void testTimeoutEventListenerException() throws Throwable {
+        expectedEx.expect(AssertionError.class);
+        expectedEx.expectMessage(CoreMatchers.containsString("Got message: Timeout message"));
+
+        this.testUtils.listenTimeout(() -> {
+                    this.testUtils.getClient().sendMessage("Timeout message");
+                }, new StandaloneEventListener<>(MessageChannelEvent.Chat.class,
+                        (MessageChannelEvent.Chat event) -> Assert.fail("Got message: " + event.getRawMessage().toPlain())),
+                20);
+    }
+
+}
