51cd74fb3d34de99267041e9b2fc70c3a03b1e8a
Olivier Brissez <OBrissez@voyages-sncf.com>
2018-07-26 13:59:09 +0200

    Beginning of test function testMineProtectedBlock

--- src/test/java/com/onaple/harvester/ReloadTest.java
+++ src/test/java/com/onaple/harvester/ReloadTest.java
@@ -1,18 +1,21 @@
 package com.onaple.harvester;
 
 import com.flowpowered.math.vector.Vector3d;
-import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.block.BlockTypes;
 import org.spongepowered.api.event.message.MessageChannelEvent;
+import org.spongepowered.api.item.ItemType;
+import org.spongepowered.api.item.ItemTypes;
+import org.spongepowered.api.item.inventory.ItemStack;
 import org.spongepowered.mctester.api.junit.MinecraftRunner;
 import org.spongepowered.mctester.internal.BaseTest;
 import org.spongepowered.mctester.internal.event.StandaloneEventListener;
 import org.spongepowered.mctester.junit.TestUtils;
-import scala.collection.concurrent.Debug;
 
 @RunWith(MinecraftRunner.class)
 public class ReloadTest extends BaseTest {
@@ -38,5 +41,26 @@ public class ReloadTest extends BaseTest {
                               || event.getMessage().toPlain().contains(dropsReloadedString));
         }));
     }
+
+    /**
+     * Mining protected block
+     */
+    /*@Test
+    public void testMineProtectedBlock() throws Throwable {
+        this.testUtils.getThePlayer().getInventory().offer(ItemStack.of(ItemTypes.STONE, 1));
+        Vector3d blockPosition = this.testUtils.getThePlayer().getPosition().add(new Vector3d(2, 5, 0));
+        this.testUtils.getClient().lookAt(blockPosition);
+        this.testUtils.getClient().rightClick();
+        this.testUtils.getThePlayer().getInventory().offer(ItemStack.of(ItemTypes.DIAMOND_PICKAXE, 1));
+        this.testUtils.getClient().selectHotbarSlot(1);
+        this.testUtils.sleepTicks(300);
+
+        this.testUtils.listenOneShot(() -> {
+            Sponge.getServer().getWorld(Sponge.getServer().getDefaultWorldName()).ifPresent(world -> {
+                Assert.assertEquals(BlockTypes.STONE, world.getBlockType(blockPosition.toInt()));
+            });
+        }, new StandaloneEventListener<>(MessageChannelEvent.class, (MessageChannelEvent event) -> {
+        }));
+    }*/
 
 }
