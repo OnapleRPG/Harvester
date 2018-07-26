6520f7e45300983009858aa1bd90c2b5b634c33e
zessirb <zessirb@gmail.com>
2018-07-26 19:13:56 +0200

    Non operationnal testMineProtectedBlock

--- src/test/java/com/onaple/harvester/HarvesterTest.java
+++ src/test/java/com/onaple/harvester/HarvesterTest.java
@@ -10,6 +10,7 @@ import org.junit.runner.RunWith;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.block.BlockTypes;
 import org.spongepowered.api.event.message.MessageChannelEvent;
+import org.spongepowered.api.event.block.ChangeBlockEvent;
 import org.spongepowered.api.item.ItemTypes;
 import org.spongepowered.api.item.inventory.ItemStack;
 import org.spongepowered.mctester.api.junit.MinecraftRunner;
@@ -55,24 +56,29 @@ public class HarvesterTest extends BaseTest {
      */
     @Test
     public void testMineProtectedBlock() throws Throwable {
-        this.testUtils.getThePlayer().getInventory().offer(ItemStack.of(ItemTypes.STONE, 1));
-        Vector3d blockPosition = this.testUtils.getThePlayer().getPosition().add(new Vector3d(2, -1, 0));
-        this.testUtils.getClient().lookAt(blockPosition);
-        /*this.testUtils.getClient().holdRightClick(true);
-        this.testUtils.sleepTicks(7);
-        this.testUtils.getClient().holdRightClick(false);
-        this.testUtils.getThePlayer().getInventory().offer(ItemStack.of(ItemTypes.DIAMOND_PICKAXE, 1));
-        this.testUtils.getClient().selectHotbarSlot(1);*/
-        this.testUtils.sleepTicks(200);
-        try {
-            this.testUtils.runOnMainThread(() -> {
-                Sponge.getServer().getWorld(Sponge.getServer().getDefaultWorldName()).ifPresent(world -> {
-                    Assert.assertEquals(BlockTypes.GRASS, world.getBlockType(new Vector3i(0, 3, 0)));
+
+        this.testUtils.listenOneShot(() -> {
+            this.testUtils.getThePlayer().getInventory().offer(ItemStack.of(ItemTypes.STONE, 1));
+            Vector3d blockPosition = this.testUtils.getThePlayer().getPosition().add(new Vector3d(2, -1, 0));
+            this.testUtils.getClient().lookAt(blockPosition);
+            this.testUtils.getClient().rightClick();
+            this.testUtils.getThePlayer().getInventory().offer(ItemStack.of(ItemTypes.DIAMOND_PICKAXE, 1));
+            this.testUtils.getClient().selectHotbarSlot(1);
+            try {
+                this.testUtils.runOnMainThread(() -> {
+                    Sponge.getServer().getWorld(Sponge.getServer().getDefaultWorldName()).ifPresent(world -> {
+                        Assert.assertEquals(BlockTypes.STONE, world.getBlockType(blockPosition.toInt().add(0, 1, 0)));
+                    });
                 });
-            });
-        } catch (Throwable e) {
-            Assert.fail(e.getMessage());
-        }
+            } catch (Throwable e) {
+                Assert.fail(e.getMessage());
+            }
+            this.testUtils.sleepTicks(200);
+        }, new StandaloneEventListener<>(ChangeBlockEvent.Break.class, (ChangeBlockEvent.Break event) -> {
+
+            })
+        );
+
     }
 
 }
